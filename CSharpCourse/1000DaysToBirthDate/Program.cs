c9417d09700d883a595def81f23b60c036b3b6a3
Łukasz Chmiel <chmiel_lukasz@interia.pl>
2019-04-11 18:47:39 +0200

    Another lessons

---
+++ CSharpCourse/1000DaysToBirthDate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1000DaysToBirthDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime birth = DateTime.Parse(Console.ReadLine());
+            birth = birth.AddDays(1000);
+            Console.WriteLine(birth.ToShortDateString());
+
+
+        }
+    }
+}
---
+++ CSharpCourse/RadiansToDegree/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace RadiansToDegree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float radians = float.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Round((180f/Math.PI)*radians, 0));
+        }
+    }
+}
